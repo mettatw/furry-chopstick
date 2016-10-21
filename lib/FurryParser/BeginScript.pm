3de135fc84245be668d9304632137e5d18618b45
Mettatw <mettatw@users.noreply.github.com>
2016-10-21 11:36:49 +0800

    More plugins for building shell scripts

---
+++ lib/FurryParser/BeginScript.pm
@@ -0,0 +1,51 @@
+#!/usr/bin/env perl
+# The tag to start a script
+#***************************************************************************
+#  Copyright 2014-2016, mettatw
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#***************************************************************************
+
+package FurryParser::BeginScript;
+
+use strict;
+use warnings;
+use v5.14;
+
+# Pattern
+# After args definition, before main script:
+# !@begin-script
+
+my $pat = qr{
+  (?: [\n\r] | ^ ) \s* \K    # look-behind non-capture, start with new line
+  !\@begin-script     # The command we want
+  \s* (?: (?= [\n\r] | $ ))  # look-ahead the end of command
+}msx; # x allow comment; s treat as single line; m multiline
+
+sub doParse {
+  my ($rhContent, $rhVariable, $rhDependency) = (@_);
+  foreach my $key (grep { $_ =~ /^text:/ } keys %{$rhContent}) {
+    my $nameUnit = (split(/:/, $key, 2))[1];
+    if ($rhContent->{$key} =~ /$pat/) {
+      my $rslt = "!>tmpl/fc-base/sh-beforemain.sh";
+      substr($rhContent->{$key}, $-[0], $+[0]-$-[0], $rslt);
+
+      # Prepend and Append template files
+      $rhContent->{$key} = "!>tmpl/fc-base/sh-header.sh\n"
+      . $rhContent->{$key} . "\n"
+      . "!>tmpl/fc-base/sh-aftermain.sh\n";
+    }
+  }
+}
+
+1;
---
+++ lib/FurryParser/ImportShell.pm
@@ -0,0 +1,58 @@
+#!/usr/bin/env perl
+# Directly import some other scripts
+#***************************************************************************
+#  Copyright 2014-2016, mettatw
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#***************************************************************************
+
+package FurryParser::ImportShell;
+
+use strict;
+use warnings;
+use v5.14;
+
+# Pattern
+# Source the file as a fd in shell
+# !.template/path
+# Export the function of this external file
+# !#template/path
+
+my $pat = qr{
+  (?: \s | ^ ) \K    # look-behind non-capture, start without non-space
+  ! ([.#]) ([^\s;:]+)     # The command we want
+  (?: (?= [\s;:] | $ ))  # look-ahead the end of command
+}msx; # x allow comment; s treat as single line; m multiline
+
+sub doParse {
+  my ($rhContent, $rhVariable, $rhDependency) = (@_);
+  foreach my $key (grep { $_ =~ /^text:/ } keys %{$rhContent}) {
+    my $nameUnit = (split(/:/, $key, 2))[1];
+    while ($rhContent->{$key} =~ /$pat/g) {
+      my $symbol = $1;
+      my $fname = $2;
+      my $rslt = "";
+      if ($symbol eq ".") {
+        $rslt = "<(~GET!$fname)"
+      } else {
+        $rslt = "declare -f \"~GET!$fname\"";
+      }
+      substr($rhContent->{$key}, $-[0], $+[0]-$-[0], $rslt);
+
+      $rhDependency->{$fname} = 1;
+      $rhVariable->{'toImport'}{$fname} = 1;
+    }
+  }
+}
+
+1;
