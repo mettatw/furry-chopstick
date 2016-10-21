d2dda08974a5f16d65f57f68318995d94f43bff3
Mettatw <mettatw@users.noreply.github.com>
2016-10-21 13:41:42 +0800

    Bundle files, make script actually work

--- tmpl/fc-base/bundleFiles.sh
+++ tmpl/fc-base/bundleFiles.sh
@@ -1,29 +1,16 @@
-#function packFile {
-#  local fname="$1"
+[%- PERL %]
+# Collect all required bundles
+my $dataAll = $stash->get('dataAll');
+my %bundles = ( map { %{$dataAll->{$_}{'toImport'}} } 
+  (grep { exists $dataAll->{$_}{'toImport'} } keys %$dataAll) );
+$stash->set('neededBundles', \%bundles);
+[% END -%]
+[%- FOREACH entry IN neededBundles -%]
+function ~GET![% entry.key %] { # {{{
+cat <<"EOF__::[% entry.key %]"
+[% fname = entry.key -%]
+[%- INCLUDE "$fname:out:main" %]
+EOF__::[% entry.key %]
+} # }}}
 
-#  echo "function ~GET!$fname { # {{{"
-#  if [[ $fname == *.xz || $fname == *.?xz ]]; then
-#    echo "base64 -d <<\"EOF__::$fname\" | xz -dc"
-#  else
-#    echo "cat <<\"EOF__::$fname\""
-#  fi # end if special compressed script
-
-#  if [[ -n "${pogb_fileComment["$fname"]:-}" ]]; then
-#    printf '%s\n' "${pogb_fileComment["$fname"]}"
-#  fi
-#  printf '%s\n' "${pogb_fileContent["$fname"]}"
-
-#  cat <<EOF
-#EOF__::$fname
-#} # }}}
-#EOF
-#  echo
-#}
-
-#for fname in "${!pogb_fileSource[@]}"; do
-#  if [[ "${pogb_fileSource[$fname]}" == 0 ]]; then
-#    continue
-#  fi
-
-#  packFile "$fname"
-#done
+[% END -%]
--- tmpl/fc-base/sh-aftermain.sh
+++ tmpl/fc-base/sh-aftermain.sh
@@ -4,6 +4,6 @@ else
 
 # Other things bundled inside this script
 
-#!>tmpl/fc-base//bundleFiles.sh
+!>tmpl/fc-base/bundleFiles.sh
 
 fi # end the really weird outer if
